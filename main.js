7461296445f5c1cb660db51817de3c8c0f72ae93
fareibrahim <faresibrahim@gmail.com>
2022-10-16 09:58:49 +0200

    here im learning git

---
+++ main.js
@@ -0,0 +1,9 @@
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("mouseover",(event)=>{
+    const x = (event.pageX - btnEl.offsetLeft);
+    const y = (event.pagey - btnEl.offsetTop);
+
+    btnEl.style.setProperty("--xpos", x + "px");
+    btnEl.style.setProperty("--ypos", y + "px");
+})
